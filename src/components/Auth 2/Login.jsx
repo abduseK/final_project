59e6e326e4cdfcccaaa3595cf40150f46f9ffaad
abdelselam kemal <abdelselamkemal74@gmail.com>
2023-05-31 22:39:18 -0700

    update #10

--- src/components/Auth 2/Login.jsx
+++ src/components/Auth 2/Login.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import axios from "axios";
+
+// we need a state to contain a list of the data fetched
 
 export default function Login() {
+  // const [post, setPost] = useState("");
+  // function getPosts() {
+
+  //   axios.get(baseUrl).then((response) => {
+  //     console.log(response.data[0]);
+  //   });
+  // }
+
   return (
     <div className="login-container">
       <h1>Login Page</h1>
