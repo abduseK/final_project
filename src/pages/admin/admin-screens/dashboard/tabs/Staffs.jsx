c48dfe987d2ab550def262070f754ecd7b5ecd30
abdelselam kemal <abdelselamkemal74@gmail.com>
2023-06-17 16:48:58 -0700

    search filter

--- src/pages/admin/admin-screens/dashboard/tabs/Staffs.jsx
+++ src/pages/admin/admin-screens/dashboard/tabs/Staffs.jsx
@@ -1,5 +1,5 @@
 import DataTable from "react-data-table-component";
-
+import { useState } from "react";
 import "../../pending/Pending.css";
 export default function StaffUsers() {
   const columns = [
@@ -18,10 +18,10 @@ export default function StaffUsers() {
       cell: (row) => (
         <div className="table-btns">
           <p className="table-btn-1" onClick={() => handleConfirm(row)}>
-            Confirm
+            Edit
           </p>
           <span className="table-btn-2" onClick={() => handleDeny(row)}>
-            Deny
+            Delete
           </span>
         </div>
       ),
@@ -57,9 +57,21 @@ export default function StaffUsers() {
       role: "REP",
     },
   ];
+  const [records, setRecords] = useState(data);
+
+  function handleFilter(event) {
+    const newData = data.filter((row) => {
+      return row.name.toLowerCase().includes(event.target.value.toLowerCase());
+    });
+
+    setRecords(newData);
+  }
   return (
     <div className="table-container">
-      <DataTable columns={columns} data={data} selectableRows></DataTable>
+      <div className="text-end">
+        <input type="text" onChange={handleFilter} />
+      </div>
+      <DataTable columns={columns} data={records} selectableRows></DataTable>
     </div>
   );
 }
